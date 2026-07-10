2eb915c0ea10258c7dc94da776d0321d4f14a35c
soulpaper <soulofpaper@gmail.com>
2026-07-10 18:08:18 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE = 'preflop-v1';
+const ASSETS = [
+  './', './index.html', './app.js',
+  './react.production.min.js', './react-dom.production.min.js',
+  './manifest.webmanifest', './icon-192.png', './icon-512.png', './icon-mask.png'
+];
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((ks) => Promise.all(ks.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+});
